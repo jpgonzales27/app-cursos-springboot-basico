61c39326043aa2d159e474b7b962e6402635b17e
Juan Pablo Gonzales <juampi7237@gmail.com>
2023-12-09 22:01:35 -0400

    paginacion y busqueda

--- src/main/java/com/juanpi/gestioncursos/controller/CursoController.java
+++ src/main/java/com/juanpi/gestioncursos/controller/CursoController.java
@@ -6,15 +6,22 @@ import com.juanpi.gestioncursos.reports.CursoExporterPDF;
 import com.juanpi.gestioncursos.repository.CursoRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -25,33 +32,57 @@ public class CursoController {
     private CursoRepository cursoRepository;
 
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "redirect:/cursos";
     }
 
     @GetMapping("/cursos")
-    public String listarCursos(Model model){
-        List<Curso> cursos = cursoRepository.findAll();
-        model.addAttribute("cursos",cursos);
+    public String listarCursos(Model model, @Param("keyword") String keyword, @RequestParam(defaultValue = "1") int page,@RequestParam(defaultValue = "3") int size){
+        try{
+            List<Curso> cursos = new ArrayList<>();
+            Pageable paging = PageRequest.of(page-1,size);
+
+            Page<Curso> pageCursos = null;
+
+            if(keyword == null){
+                pageCursos = cursoRepository.findAll(paging);
+            }
+            else{
+                pageCursos = cursoRepository.findByTituloContainingIgnoreCase(keyword,paging);
+                model.addAttribute("keyword",keyword);
+            }
+
+            cursos = pageCursos.getContent();
+
+            model.addAttribute("cursos",cursos);
+
+            model.addAttribute("currentPage",pageCursos.getNumber() + 1);
+            model.addAttribute("totalItems",pageCursos.getTotalElements());
+            model.addAttribute("totalPages",pageCursos.getTotalPages());
+            model.addAttribute("pageSize",size);
+        }catch (Exception exception){
+            model.addAttribute("message",exception.getMessage());
+        }
+
         return "cursos";
     }
 
     @GetMapping("/cursos/nuevo")
-    public String agregarCurso(Model model){
+    public String agregarCurso(Model model) {
         Curso curso = new Curso();
         curso.setPublicado(true);
-        model.addAttribute("curso",curso);
-        model.addAttribute("pageTitle","Nuevo Curso");
+        model.addAttribute("curso", curso);
+        model.addAttribute("pageTitle", "Nuevo Curso");
         return "curso_form";
     }
 
     @PostMapping("/cursos/nuevo")
-    public String guardarCurso(Curso curso, RedirectAttributes redirectAttributes){
-        try{
+    public String guardarCurso(Curso curso, RedirectAttributes redirectAttributes) {
+        try {
             cursoRepository.save(curso);
-            redirectAttributes.addFlashAttribute("message","El curso ha sido guardado con exito");
-        }catch (Exception e){
-            redirectAttributes.addAttribute("message","El curso no ha sido guardado");
+            redirectAttributes.addFlashAttribute("message", "El curso ha sido guardado con exito");
+        } catch (Exception e) {
+            redirectAttributes.addAttribute("message", "El curso no ha sido guardado");
         }
 
         return "redirect:/cursos";
@@ -65,7 +96,7 @@ public class CursoController {
 
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=cursos" + currentDateTime + ".pdf";
-        response.setHeader(headerKey,headerValue);
+        response.setHeader(headerKey, headerValue);
 
         List<Curso> cursos = cursoRepository.findAll();
 
@@ -82,7 +113,7 @@ public class CursoController {
 
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=cursos" + currentDateTime + ".xlsx";
-        response.setHeader(headerKey,headerValue);
+        response.setHeader(headerKey, headerValue);
 
         List<Curso> cursos = cursoRepository.findAll();
 
@@ -90,5 +121,6 @@ public class CursoController {
         exporterExcel.export(response);
 
     }
+
 
 }
